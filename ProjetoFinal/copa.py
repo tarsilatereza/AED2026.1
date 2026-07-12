003f71bdfad905ed91bcbac977c599177f1e87a1
tarsilatereza <tarsilatereza@gmail.com>
2026-07-11 21:17:04 -0300

    Operações do MENU concluídas

--- ProjetoFinal/copa.py
+++ ProjetoFinal/copa.py
@@ -41,27 +41,57 @@ while True:
         }
         print("Seleção adicionada com sucesso!")
         
-
-
-
     # Opção para editar dicas e nomes das seleções já existentes
-    #elif opcao == '3':
-        
+    # Obs.3: Consultei IA para entender como editar elementos do dicionário "dados.py"
+    elif opcao == '3':
+        print("="*9 + " EDITAR SELEÇÃO " + "="*9)
+        pais = input("Digite o nome da seleção que deseja editar: ").title()
+        if pais in selecoes:
+            print(f"Editando os dados de {pais}.")
+            print("ATENÇÃO: deixe em branco e aperte [Enter] para manter os dados atuais.")
 
-    elif opcao == '4':
-            cod = input("Informe o código que deseja editar ")
+            atual_grupo = selecoes[pais]['grupo']
+            novo_grupo = input(f"Grupo atual [{atual_grupo}]: ").upper()
+            if novo_grupo != "":
+                selecoes[pais]['grupo'] = novo_grupo
+            atual_titulos = selecoes[pais]['titulos']
+            novo_titulos = input(f"Títulos atuais [{atual_titulos}]: ")
+            if novo_titulos != "":
+                selecoes[pais]['titulos'] = int(novo_titulos)
+            atual_continente = selecoes[pais]['continente']
+            novo_continente = input(f"Continente atual [{atual_continente}]: ").title()
+            if novo_continente != "":
+                selecoes[pais]['continente'] = novo_continente
+            atual_lingua = selecoes[pais]['lingua']
+            novo_lingua = input(f"Língua atual [{atual_lingua}]: ").title()
+            if novo_lingua != "":
+                selecoes[pais]['lingua'] = novo_lingua
+                print(f"Os dados de {pais} foram atualizados!")
 
-            novo_codigo = input(f"Digite o código ou enter para manter '{cod}'")
-            novo_nome = input(f"Digite o NOME ou enter para manter '{selecoes[cod]}'")
-
-            if cod != novo_codigo:
-                selecoes[novo_codigo] = novo_nome
-                del selecoes[cod]
+            print("="*20)
+    
+            print(f"Essas são as novas dicas da seleção: {selecoes[pais].items()}")
+            
+        else:
+            print(f"ATENÇÃO! A seleção '{pais}' não está cadastrada.")
+            nova_opcao = input("Deseja ir para a tela de adicionar seleção [S/N]? ")
+            if nova_opcao.upper() == "S":
+                print("Redirecionando para o MENU. Digite a opção 2 no menu a seguir.")
+                continue # Uso do 'continue' como sugestão de IA, não conhecia
             else:
-                selecoes[cod] = novo_nome
+                print("\n-> Operação cancelada. Voltando ao menu principal...")
+                continue       
 
-    elif opcao == '5':
-        cod = input("Informe o código que deseja deletar ")
-        # VERIFICAR SE cod existe 
-        # del
-        # print( ) - > Deletado com sucesso
+    # Opção para remover seleções da lista
+    elif opcao == '4':
+        pais = input("Informe o nome da seleção que deseja eliminar: ").title()
+        if pais in selecoes:
+            del selecoes[pais]
+            print(f"{pais} deletado")
+        else:
+            print("Este país não está no sistema. Por favor, verifique a grafia ou adicione uma nova seleção.")
+
+# Fim das operações do MENU
+
+
+
